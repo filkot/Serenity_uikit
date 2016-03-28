60ea789861327511254cfadd9840d1c867c4f324
mj <mj2k@yandex.ru>
2016-03-28 15:04:16 +0300

    ButtonSteps and ButtonMappingSteps are updated

--- src/test/java/com/axiomsl/serenity/steps/ButtonMappingSteps.java
+++ src/test/java/com/axiomsl/serenity/steps/ButtonMappingSteps.java
@@ -5,33 +5,34 @@ import net.thucydides.core.annotations.Steps;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
-
 public class ButtonMappingSteps {
 
+    //region @Steps
     @Steps
     ButtonSteps step;
+    //endregion @Steps
 
-    //WHEN
-    @When("the user click button with name '$buttonName'")
-    public void whenTheUserClickTheButton(String buttonName) {
+    //region @When
+    @When("the user clicks on '$buttonName' button")
+    public void whenTheUserClicksOnTheButton(String buttonName) {
         step.click_button_by_name(buttonName);
     }
 
-    @When("the user does a right click button with name '$buttonName'")
-    public void whenTheUserRightClickTheButton(String buttonName) {
+    @When("the user right clicks on '$buttonName' button")
+    public void whenTheUserRightClicksOnTheButton(String buttonName) {
         step.right_click_button_by_name(buttonName);
     }
 
-    @When("the user hovers on button with name '$buttonName'")
+    @When("the user hovers on '$buttonName' button")
     public void whenTheUserHoverOnTheButton(String buttonName) {
         step.hover_on_button_by_name(buttonName);
     }
+    //endregion @When
 
-    //THEN
-    @Then("they should see the button with text '$text'")
-    public void thenTheyShouldSeeAButtonWithText(String text) {
-        step.should_see_button_with_text(text);
+    //region @Then
+    @Then("the user should see the button with '$name' name")
+    public void thenTheUserShouldSeeButtonWithName(String name) {
+        step.should_see_button_with_name(name);
     }
-
-
+    //endregion @Then
 }
