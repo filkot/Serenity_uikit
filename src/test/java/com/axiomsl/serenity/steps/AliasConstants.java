beeefae3a77f30e0aa52ddd3262964f070d56a2d
dbudranov <dbudranov@axiomsl.com>
2016-04-01 20:41:06 +0300

    incomplited stories were deleted

---
+++ src/test/java/com/axiomsl/serenity/steps/AliasConstants.java
@@ -0,0 +1,10 @@
+package com.axiomsl.serenity.steps;
+
+/**
+ * Created by dbudranov on 29.03.2016.
+ */
+public class AliasConstants {
+    public final static String ALS_TM_SHOULD_NOT_1 = "the user should not see the row with <columnName> column and <cellValue> value in table";
+    public final static String ALS_TM_SHOULD_NOT_2 = "the user should not see the row with <columnName> column and <cellValue> value in $tableCaption table";
+    public final static String ALS_TM_SHOULD_NOT_3 = "the user should not see the row with '$columnName' column and '$cellValue' value in <tableCaption> table";
+}
